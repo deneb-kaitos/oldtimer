9f4b6cb85b39f7aeca5209cfe93aae3549702a48
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2023-10-09 14:12:14 +0200

    added initial layout

--- sources/front-end/src/routes/+page.svelte
+++ sources/front-end/src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  export const prerender = true;
+
+  import Slogan from '../mainPageSections/Slogan.svelte';
+  import Links from '../mainPageSections/Links.svelte';
+  import Contacts from '../mainPageSections/contacts/Contacts.svelte';
+</script>
+<style>
+  article {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-auto-rows: 1fr;
+    grid-template-areas:
+    'slogan'
+    'links'
+    'contacts';
+
+    gap: max(1vw, 1vh);
+  }
+
+  section {
+    display: flex;
+    flex: 1 0 auto;
+  }
+
+  #slogan {
+    grid-area: slogan;
+  }
+
+  #links {
+    grid-area: links;
+  }
+
+  #contacts {
+    grid-area: contacts;
+  }
+</style>
+
+<article>
+  <section id="slogan">
+    <Slogan />
+  </section>
+  <section id="links">
+    <Links />
+  </section>
+  <section id="contacts">
+    <Contacts />
+  </section>
+</article>
