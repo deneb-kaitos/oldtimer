85ba506b3a3cd5a96d01dc2fdad4eabda23731da
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2023-10-09 23:46:57 +0200

    added contacts button to the footer

---
+++ sources/front-end/src/routes/contacts/+page.svelte
@@ -0,0 +1,14 @@
+
+<ul>
+  <li>
+    <a href="tel:017638000034" target="_blank">017638000034</a>
+  </li>
+
+  <li>
+    <a href="tel:89055585948" target="_blank">89055585948</a>
+  </li>
+
+  <li>
+    <a href="mailto:andrey.paymushin@gmail.com?subject=[business inquiry]" target="_blank">andrey.paymushin@gmail.com</a>
+  </li>
+</ul>
